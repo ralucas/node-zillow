b9ed2d6f88f11f92c2898a88a77acecf272f1fdb
R.A. Lucas <richard@richardalucas.com>
2014-05-05 21:35:51 -0600

    moved to integration tests

--- test/node-zillow_test.js
+++ test/node-zillow_test.js
@@ -3,18 +3,7 @@
 var Zillow         = require('../lib/node-zillow.js'),
     mocha          = require('mocha'),
     chai           = require('chai'),
-    expect         = require('chai').expect,
-    assert         = require('chai').assert,
-    sinon          = require('sinon'),
-    chaiAsPromised = require('chai-as-promised'),
-    util           = require('util'),
-    env            = require('../environment.json');
-
-// for chai promises
-chai.use(chaiAsPromised);
-
-// test constants
-var zwsid = env.zwsid;
+    expect         = require('chai').expect;
 
 describe('Zillow', function() {
 
@@ -25,59 +14,8 @@ describe('Zillow', function() {
     });
     describe('an instance of', function() {
         it('zillow should be an instanceof Zillow', function() {
-            var zillow = new Zillow(zwsid);
+            var zillow = new Zillow('xxx');
             expect(zillow).to.be.an.instanceof(Zillow);
         });
     });
 });
-
-describe('getDeepSearchResults', function() {
-    var zillow = new Zillow(zwsid);
-    var parameters = {
-        address: env.address,
-        citystatezip: env.citystatezip
-    };
-    var test = zillow.getDeepSearchResults(parameters);
-
-    it('should return a json object', function() {
-        expect(typeof test.then(function(result) {
-            result.to.equal('object');
-        }));
-    });
-
-    it('should return a success message', function() {
-        expect(test.then(function(result) {
-            result['message']['text'].to.equal
-            ('Request successfully processed');
-        }));
-        expect(test.then(function(result) {
-            result['message']['code'].to.equal(0);
-        }));
-    });
-});
-
-describe('getDemographics', function() {
-    var zillow = new Zillow(zwsid);
-    var params = {
-        city: env.city,
-        state: env.state,
-        zip: env.zip
-    };
-    var test = zillow.getDemographics(params);
-
-    it('should be a json object', function() {
-        expect(typeof test.then(function(result) {
-            result.to.equal('object');
-        }));
-    });
-
-    it('should return a success message', function() {
-        expect(test.then(function(result) {
-            result['message']['text'].to.equal
-            ('Request successfully processed');
-        }));
-        expect(test.then(function(result) {
-            result['message']['code'].to.equal(0);
-        }));
-    });
-})
