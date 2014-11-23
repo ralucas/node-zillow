c8929efc0d081dc29915b82b7de1e62d83807bb9
R.A. Lucas <richard@richardalucas.com>
2014-11-23 13:26:25 -0700

    updating

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
           reporter: 'spec',
           require: 'coverage/blanket'
         },
-        src: ['test/**/*_test.js']
+        src: ['test/*.js']
       },
       coverage: {
         options: {
